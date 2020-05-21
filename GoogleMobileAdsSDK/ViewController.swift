4e9a7f95e2bb6c95914d48f761bd737a02bb30e9
Bilgihan Kose <bilgihankose@gmail.com>
2020-05-21 12:56:10 +0300

    fix view.safeAreaLayoutGuide.bottomAnchor

--- GoogleMobileAdsSDK/ViewController.swift
+++ GoogleMobileAdsSDK/ViewController.swift
@@ -10,55 +10,55 @@ import GoogleMobileAds
 import UIKit
 
 class ViewController: UIViewController, GADBannerViewDelegate {
-
-    var bannerView: DFPBannerView!
-
-
-  var interstitial: DFPInterstitial!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    interstitial = DFPInterstitial(adUnitID: "/6499/example/interstitial")
-    let request = DFPRequest()
-    interstitial.load(request)
-   
-    bannerView = DFPBannerView(adSize: kGADAdSizeBanner)
-
-    addBannerViewToView(bannerView)
-    bannerView.adUnitID = "/6499/example/banner"
-    bannerView.rootViewController = self
-     bannerView.load(DFPRequest())
-      bannerView.delegate = self
     
-  }
+    var bannerView: DFPBannerView!
+    
+    
+    var interstitial: DFPInterstitial!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        interstitial = DFPInterstitial(adUnitID: "/6499/example/interstitial")
+        let request = DFPRequest()
+        interstitial.load(request)
+        
+        bannerView = DFPBannerView(adSize: kGADAdSizeBanner)
+        
+        addBannerViewToView(bannerView)
+        bannerView.adUnitID = "/6499/example/banner"
+        bannerView.rootViewController = self
+        bannerView.load(DFPRequest())
+        bannerView.delegate = self
+        
+    }
     @IBAction func adsDownload(_ sender: UIButton) {
         
         if interstitial.isReady {
-          interstitial.present(fromRootViewController: self)
+            interstitial.present(fromRootViewController: self)
         } else {
-          print("Ad wasn't ready")
+            print("Ad wasn't ready")
         }
     }
     
     func addBannerViewToView(_ bannerView: DFPBannerView) {
-     bannerView.translatesAutoresizingMaskIntoConstraints = false
-     view.addSubview(bannerView)
-     view.addConstraints(
-       [NSLayoutConstraint(item: bannerView,
-                           attribute: .bottom,
-                           relatedBy: .equal,
-                           toItem: bottomLayoutGuide,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: 0),
-        NSLayoutConstraint(item: bannerView,
-                           attribute: .centerX,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .centerX,
-                           multiplier: 1,
-                           constant: 0)
-       ])
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: view.safeAreaLayoutGuide.bottomAnchor,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+        ])
     }
     
 }
